f428c929d0c253e73db96e583b358518baab5e2f
Pankratov_Pavlo <p.p.1999027@gmail.com.com>
2019-12-20 22:26:27 +0200

    upload zuul

---
+++ src/main/java/com/components/gatewayserver/security/SecurityTokenConfig.java
@@ -0,0 +1,33 @@
+package com.components.gatewayserver.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import javax.servlet.http.HttpServletResponse;
+
+
+@EnableWebSecurity    // Enable security config. This annotation denotes config for spring security.
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.jwt.uri}")
+    private String jwtUri = "/login/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilterAfter(new JwtTokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(jwtUri).permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
